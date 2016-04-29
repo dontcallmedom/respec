9390ef305ca135d4812eb3629c1aa8cc6879ae98
Dominique Hazael-Massieux <dom@w3.org>
2016-04-29 11:32:04 +0200

    Adapt build script to new respec

--- tools/build-webidlconvert.js
+++ tools/build-webidlconvert.js
@@ -1,22 +1,42 @@
 #!/usr/local/bin/node
 
-var fs   = require("fs")
-,   pth  = require("path")
-,   b    = require("./builder")
-,   version = JSON.parse(fs.readFileSync(pth.join(__dirname, "../package.json"), "utf-8")).version
-,   builds = pth.join(__dirname, "../builds")
-,   latest = pth.join(builds, "respec-webidlconvert.js")
-;
+"use strict";
+const async = require("marcosc-async");
+const Builder = require("./builder").Builder;
+const path = require("path");
+const colors = require("colors");
 
-function build (cb) {
-    var opts = { out: latest, name: "profile-webidlconvert" };
-    b.build(opts, cb);
-}
+colors.setTheme({
+  data: "grey",
+  debug: "cyan",
+  error: "red",
+  help: "cyan",
+  info: "green",
+  input: "grey",
+  prompt: "grey",
+  verbose: "cyan",
+  warn: "yellow",
+});
+
+const buildWebidlConvert = async(function*() {
+  let versionsToBuild = Array.from(arguments);
+  if (!versionsToBuild.length){
+    versionsToBuild.push("latest");
+  }
+  const builds = path.join(__dirname, "../builds");
+  for(let aVersion of versionsToBuild){
+    const isLatest = aVersion === "latest";
+    const version = (isLatest) ? yield Builder.getRespecVersion() : aVersion;
+    const outFile = "respec-webidlconvert.js" + ((isLatest) ? ".js" : `-${aVersion}.js`);
+    const out = path.join(builds, outFile);
+    console.log(colors.info(`  ⏲  Generating ${outFile}. Please wait...`));
+      yield Builder.build({out, version, name: "profile-webidlconvert"});
+  }
+});
 
 if (require.main === module) {
-    build(true, function () {
-        console.log("OK!");
-    });
+  buildWebidlConvert("latest")
+    .catch((err) => console.log(colors.error(err.stack)));
 }
 
-exports.build = build;
+exports.buildWebidlConvert = buildWebidlConvert;
