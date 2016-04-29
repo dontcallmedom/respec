23374f21a0e3b56f7199f9538aab486fff4b4182
Dominique Hazael-Massieux <dom@w3.org>
2016-04-29 15:23:24 +0200

    Adapt to new profile model

--- js/profile-webidlconvert.js
+++ js/profile-webidlconvert.js
@@ -1,32 +1,54 @@
-/*global respecVersion */
-
+"use strict";
 // this is only set in a build, not at all in the dev environment
-var requireConfig = {
-    shim:   {
-        shortcut: {
-            exports:    "shortcut"
-        }
-    }
-};
-if ("respecVersion" in window && respecVersion) {
-    requireConfig.paths = {
-        ui:   "https://w3c.github.io/respec/js/ui"
-    };
-}
-require.config(requireConfig);
+require.config({
+  shim: {
+    shortcut: {
+      exports: "shortcut"
+    },
+    Promise: {
+      exports: "Promise"
+    },
+  },
+  paths: {
+    "fetch": "/node_modules/whatwg-fetch/fetch",
+    "handlebars": "/node_modules/handlebars/dist/handlebars",
+    "jquery": "/node_modules/jquery/dist/jquery",
+    "Promise": "/node_modules/promise-polyfill/promise",
+    "webidl2": "/node_modules/webidl2/lib/webidl2",
+  },
+  deps: [
+    "fetch",
+    "jquery",
+    "Promise",
+    "core/jquery-enhanced",
+  ],
+});
 
 define([
+    // order is significant
             "domReady"
         ,   "core/base-runner"
         ,   "core/ui"
-        ,   "core/utils"
-        ,   "core/webidl-oldschool-converter"
-        ],
-        function (domReady, runner, ui) {
-            var args = Array.prototype.slice.call(arguments);
-            domReady(function () {
-                ui.addCommand("Save Snapshot", "ui/save-html", "Ctrl+Shift+Alt+S");
-                runner.runAll(args);
-            });
-        }
+    ,   "core/utils"
+    ,   "core/webidl-oldschool-converter"
+    ,    "ui/save-html"
+  ],
+  function(domReady, runner, ui) {
+    var args = Array.from(arguments);
+    domReady(function() {
+      ui.addCommand("Save Snapshot", "ui/save-html", "Ctrl+Shift+Alt+S");
+     // ui.addCommand("About ReSpec", "ui/about-respec", "Ctrl+Shift+Alt+A");
+     // ui.addCommand("Definition List", "ui/dfn-list", "Ctrl+Shift+Alt+D");
+     // ui.addCommand("Search Specref DB", "ui/search-specref", "Ctrl+Shift+Alt+space");
+      runner
+        .runAll(args)
+        .then(document.respectIsReady)
+        .then(ui.show)
+        .catch(function(err){
+          console.error(err);
+          // even if things go critically bad, we should still try to show the UI
+          ui.show();
+        })
+    });
+  }
 );
